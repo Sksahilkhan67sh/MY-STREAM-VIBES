a79cccbfac58a33b9efb0452362e561da5585a66
SAHIL KHAN <146698424+Sksahilkhan67sh@users.noreply.github.com>
2026-04-29 07:36:30 +0000

    fix: remove POLE FILE folder from client

--- client/.next/types/app/host/page.ts
+++ client/.next/types/app/host/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\sahil\Desktop\streamvault\client\src\app\host\page.tsx
+// File: /workspaces/MY-STREAM-VIBES/client/src/app/host/page.tsx
 import * as entry from '../../../../src/app/host/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- client/.next/types/app/layout.ts
+++ client/.next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\sahil\Desktop\streamvault\client\src\app\layout.tsx
+// File: /workspaces/MY-STREAM-VIBES/client/src/app/layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
